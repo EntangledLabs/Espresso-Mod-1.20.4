593bc9c7f250b1fc90dfb0f6dc6bc9f75e41c24c
EntangledLabs <yiclemence@gmail.com>
2024-01-26 21:51:49 -0800

    Initial commit

---
+++ src/main/java/net/entangled/espressomod/EspressoModClient.java
@@ -0,0 +1,10 @@
+package net.entangled.espressomod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EspressoModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
